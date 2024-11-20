2a898ca08adc91cf8ad1cf7f52075a7e7ad00280
Ashijain25 <91389729+Ashijain25@users.noreply.github.com>
2024-11-19 22:02:50 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MainNavigator from './src/navigation/MainNavigator'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const App = () => {
+  return (
+
+      <MainNavigator />
+
+  )
+}
+
+export default App
